acb45cc13601d8dcffb2e0a83c4eb15b7ac704fb
dave <zevakato@gmail.com>
2022-12-21 18:42:09 +0300

    disclaimer middle

--- src/App.js
+++ src/App.js
@@ -27,14 +27,17 @@ export default function App() {
       <CookieConsent 
         debug={true} 
         style={{
-          width: "70%", 
-          marginLeft: "15%", 
+          width: "100%",
+          height: "100vh",
           borderRadius: "2em 2em 0 0", 
           zIndex: "999999",
           alignItems: "center",
-          display: "flex",
+          display: "grid",
           justifyContent:"center",
           paddingBottom: "3em",
+          backgroundColor: "transparent",
+          backdropFilter: "blur(20px)",
+          fontSize: "xx-large"
           
           }}
 
@@ -46,14 +49,17 @@ export default function App() {
             display: "flex",
             justifyContent: "center",
             fontSize: "initial",
-            width: "80%"
-            
+            width: "40%",
+            marginLeft: "30%",
+            padding: "0",         
+            padding: "3em",
+            borderRadius: "10em"         
         }}
         contentStyle={{
           maxWidth: "100%",
           display: "flex",
           justifyContent: "center",
-          fontSize: "1.5em"
+          fontSize: "xx-large"
         }}
         
         >
